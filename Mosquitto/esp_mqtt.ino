c34a96d3a07bf4df52bf01b5ce5b25954dc48bf8
sebathorus <sebathorus@yahoo.com>
2016-06-29 17:58:39 +0300

    Add files via upload

---
+++ Mosquitto/esp_mqtt.ino
@@ -0,0 +1,188 @@
+#include <ESP8266WiFi.h>
+#include <OneWire.h>
+#include <PubSubClient.h>
+
+const char* ssid = "own_ssid"; // Your ssid
+const char* password = "own_pass"; // Your Password
+uint8_t mac[6];
+String espMAC;
+long lastMsg = 0;
+char msg[50];
+char espMAC2[12];
+int value = 23;
+char nodefeed[33];
+byte i;
+byte present = 0;
+byte type_s;
+byte data[12];
+byte addr[8];
+float celsius, fahrenheit;
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+const char* mqtt_server = "aaa.bbb.ccc.ddd";//IP of RaspberryPi broker
+
+OneWire ds(2);  // on pin 2 a 4.7K resistor is required between DS18B20 data pin and Vcc
+//WiFiServer server(80);
+
+String macToStr(const uint8_t* mac)//convert MAC address to a String
+{
+  String result;
+  for (int i = 0; i < 6; ++i) {
+    result += String(mac[i], 16);
+  }
+  return result;
+}
+
+//this function process messages arrived on "sensorsfeed/commands/espMAC" feed, where espMAC is the actual MAC address of the node detected by code
+void callback(char* topic, byte* payload, unsigned int mqttlength) {
+  String recMAC;
+  String recpayload;
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.println("] ");
+  for (int i = 0; i < mqttlength; i++) {
+    recpayload += (char)payload[i];
+  }
+  
+  //Serial.print("recMAC ");
+  //Serial.println(recMAC);
+  Serial.print("recpayload ");
+  Serial.println(recpayload);
+  
+  if (recpayload == "1") {
+      Serial.println("Turn LED ON");//if message payload is "1" this message is print on serial console
+    } else {
+    Serial.println("Turn LED Off");//otherwise, this message is print on serial console
+    } 
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  espMAC = macToStr(mac);// the MAC address of each ESP8266 is used to uniquely identify nodes
+  espMAC.toCharArray(espMAC2,13);
+  sprintf(nodefeed, "%s%s", "sensorsfeed/commands/", espMAC2);
+//  Serial.println(espMAC);
+//  Serial.println(espMAC2);
+//  Serial.println(nodefeed);
+  
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect("ESP8266Client")) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      //client.publish("sensorsfeed/messages", "hello world");
+      // ... and resubscribe
+      client.subscribe(nodefeed);//client subscribe to "sensorsfeed/commands/espMACaddress" feed and listen for commands
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}//end reconnect
+
+void tempread() {
+
+if ( !ds.search(addr)) {
+ds.reset_search();
+delay(250);
+return;
+}
+// the first ROM byte indicates which chip
+switch (addr[0]) {
+   case 0x10:
+//Serial.println("  Chip = DS18S20");  // or old DS1820
+    type_s = 1;
+    break;
+   case 0x28:
+//Serial.println("  Chip = DS18B20");
+    type_s = 0;
+    break;
+  case 0x22:
+//Serial.println("  Chip = DS1822");
+    type_s = 0;
+    break;
+  default:
+//Serial.println("Device is not a DS18x20 family device.");
+  return;
+}
+
+ds.reset();
+ds.select(addr);
+ds.write(0x44); // start conversion
+delay(1000);
+present = ds.reset();
+ds.select(addr);
+ds.write(0xBE);         // Read Scratchpad
+
+for ( i = 0; i < 9; i++) {           // we need 9 bytes
+data[i] = ds.read();
+}
+
+int16_t raw = (data[1] << 8) | data[0];
+if (type_s) {
+raw = raw << 3; // 9 bit resolution default
+if (data[7] == 0x10) {
+                      // "count remain" gives full 12 bit resolution
+                      raw = (raw & 0xFFF0) + 12 - data[6];
+                     }
+} 
+else {
+byte cfg = (data[4] & 0x60);
+// at lower res, the low bits are undefined, so let's zero them
+if (cfg == 0x00) raw = raw & ~7;  // 9 bit resolution, 93.75 ms
+else if (cfg == 0x20) raw = raw & ~3; // 10 bit res, 187.5 ms
+else if (cfg == 0x40) raw = raw & ~1; // 11 bit res, 375 ms
+//// default is 12 bit resolution, 750 ms conversion time
+    }
+celsius = (float)raw / 16.0;
+//fahrenheit = celsius * 1.8 + 32.0;
+}//end tempread
+
+void setup() {
+Serial.begin(115200);
+// Connect to WiFi network
+Serial.print("Connecting to ");
+Serial.println(ssid);
+
+WiFi.begin(ssid, password);
+while (WiFi.status() != WL_CONNECTED) {
+     delay(500);
+     Serial.print(".");
+     }
+Serial.println("");
+Serial.println("WiFi connected");
+
+Serial.println(WiFi.localIP());// Print the IP address
+WiFi.macAddress(mac);//read own ESP MAC address
+client.setServer(mqtt_server, 1883);
+client.setCallback(callback);
+}
+
+void loop() {
+
+tempread();//read temperature from sensor
+//Serial.print("Temperature = ");
+//Serial.print(celsius);
+//Serial.println(" Celsius, ");
+
+//check for MQTT client connection and attempt reconection if required
+if (!client.connected()) {
+    reconnect();
+}
+
+String espdata = "temp=" + (String)celsius + "=" + (String)espMAC; //string what is going to be sent to MQTT broker running on RaspberryPi
+client.loop();
+espdata.toCharArray(msg, 50);
+Serial.print("Publish message: ");
+Serial.println(msg);
+client.publish("sensorsfeed/temperature", msg);
+client.subscribe(nodefeed);
+
+delay(5000);
+}
---
+++ Mosquitto/pilot_mqtt.py
@@ -0,0 +1,116 @@
+import time 
+import sys
+from datetime import datetime
+from os import listdir 
+from os.path import isdir, isfile, join, exists
+import paho.mqtt.client as mqtt
+
+
+DIR_BASEINST = "/var/www/html/datainst/"
+DIR_BASEHIST = "/var/www/html/datahist/"
+nodelist = []
+pilotIP = "aaa.bbb.ccc.ddd" - RaspberryPi IP address
+receivedvaltemp = ""
+receivedvalhum = ""
+receivedvallight = ""
+
+
+
+def on_connect(mqttc, obj, flags, rc):
+	print("rc: "+str(rc))
+	#client subscribe to all feeds with names begining with "sensorfeed/"
+	mqttc.subscribe("sensorsfeed/#", 0)
+
+def on_message(mqttc, obj, msg):
+#	print everything published on "sensorfeed/" and not matching temperature, humidity, light or detection messages
+	print(str(msg.payload))
+
+def on_message_temperature(mqttc, obj, msg):
+	global receivedvaltemp
+	receivedvaltemp = str(msg.payload)
+	recsplit = receivedvaltemp.split("=")
+	tempval = recsplit[1]
+	nodemac = recsplit[2]
+	print "Node MAC from temperature feed = ", nodemac
+	tempdatahist = DIR_BASEHIST + nodemac + "_temphist"
+	tempdatainst = DIR_BASEINST + nodemac + "_tempinst"
+#	fhtdh = file handler temp data hist	- this file store a log temperature values sent by nodes
+#	if a log file for temperature data of a node did not exist, it is created - nodes can be added on the fly without previous configuration
+	if not exists(tempdatahist):
+		fhtdh = open(tempdatahist,'w')
+		fhtdh.close()
+	fhtdh = open(tempdatahist,'a')
+	fhtdh.write(time.strftime("%D-%H:%M:%S ") + tempval + '\n')
+	fhtdh.close()
+
+#   fhtdi = file handler temp data inst	- this file store only the last temperature value sent by nodes
+#	being an instantaneous value, the content of the file is overwritten every time
+	fhtdi = open(tempdatainst,'w')
+    fhtdi.write(tempval + " " + nodemac)
+    fhtdi.close()
+
+	print "Temperature ", tempval
+#        print(str(msg.payload))
+
+def on_message_humidity(mqttc, obj, msg):
+	global receivedvalhum
+        receivedvalhum = str(msg.payload)
+        recsplit = receivedvalhum.split("=")
+        humval = recsplit[1]
+        nodemac = recsplit[2]
+        print "Node MAC from humidity feed = ", nodemac
+        humdatahist = DIR_BASEHIST + nodemac + "_humhist"
+        humdatainst = DIR_BASEINST + nodemac + "_huminst"
+#       fhhdh = file handler humidity data hist
+        if not exists(humdatahist):
+                fhhdh = open(humdatahist,'w')
+                fhhdh.close()
+        fhhdh = open(humdatahist,'a')
+        fhhdh.write(time.strftime("%D-%H:%M:%S ") + humval + '\n')
+        fhhdh.close()
+
+#       fhhdi = file handler humidity data inst
+        fhhdi = open(humdatainst,'w')
+        fhhdi.write(humval)
+        fhhdi.close()
+	print "Humidity ",humval
+
+
+def on_message_light(mqttc, obj, msg):
+        print("Light intensity "+str(msg.payload))
+
+def on_message_detection(mqttc, obj, msg):
+        print("Movement detected "+str(msg.payload))
+
+def on_publish(mqttc, obj, mid):
+        print("mid: "+str(mid))
+
+def on_subscribe(mqttc, obj, mid, granted_qos):
+        print("Subscribed: "+str(mid)+" "+str(granted_qos))
+
+mqttc = mqtt.Client()
+
+mqttc.on_message = on_message
+
+# client follow and process only messages published on following feeds, by calling corresponding functions
+mqttc.message_callback_add("sensorsfeed/temperature/#", on_message_temperature)
+mqttc.message_callback_add("sensorsfeed/humidity/#", on_message_humidity)
+mqttc.message_callback_add("sensorsfeed/light/#", on_message_light)
+mqttc.message_callback_add("sensorsfeed/detection/#", on_message_detection)
+
+mqttc.on_connect = on_connect
+mqttc.on_publish = on_publish
+mqttc.on_subscribe = on_subscribe
+
+#	client connect to RaspberryPi broker
+mqttc.connect(pilotIP, 1883, 60)
+mqttc.loop_start()
+
+try:
+	while True:
+		print receivedvaltemp
+		time.sleep(10)
+
+except KeyboardInterrupt:
+       print 'Bye'
+       mqttc.loop_stop()
